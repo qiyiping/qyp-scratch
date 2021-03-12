9831913c544bb10bb15d378cd52ce893ee60b70f
Yiping Qi <qiyiping@gmail.com>
2021-03-12 19:49:56 +0800

    try cmake

--- scratch/sieve/sieve_test.cpp
+++ scratch/sieve/sieve_test.cpp
@@ -4,16 +4,19 @@
 
 int main(int argc, char *argv[]) {
   int ith = 100000;
-  int64_t t1 = base::GetMicroSeconds();
-  PrimeGenerator<long long> p;
-  for (int i = 0; i < ith-1; ++i) {
-    p.Next();
-    // std::cout << p.Next() << " ";
+  if (argc > 1) {
+    ith = atoi(argv[1]);
   }
-  std::cout << p.Next() << std::endl;
-  std::cout << base::GetMicroSeconds() - t1 << std::endl;
+  int64_t t1 = base::GetMicroSeconds();
+  // PrimeGenerator<long long> p;
+  // for (int i = 0; i < ith-1; ++i) {
+  //   p.Next();
+  //   // std::cout << p.Next() << " ";
+  // }
+  // std::cout << p.Next() << std::endl;
+  // std::cout << base::GetMicroSeconds() - t1 << std::endl;
 
-  t1 = base::GetMicroSeconds();
+  // t1 = base::GetMicroSeconds();
   PrimeGenerator2<long long> p2;
   for (int i = 0; i < ith-1; ++i) {
     p2.Next();
